021712f833faba3fc778fe8a99bd97533d1ec107
Samet <91340117+s4mto@users.noreply.github.com>
2021-11-14 16:34:56 +0100

    Update basic_import.py

--- my_dice.py/basic_import.py
+++ my_dice.py/basic_import.py
@@ -1,15 +1,15 @@
 import dice_percentage
 
 while True : 
-    print("press q for exit")
+    print("Please press Q for exit")
     repetition = input("repetition : ")
-    if repetition == "q" or repetition == "Q":
+    if repetition == "Q" or repetition == "p":
         break
     else : 
         repetition = int(repetition)
-        print(f"percentage of throws of value 1 = {dicepercentage.percentage(1,repetition):.2f} %") # we wanted only 2 decimal . in print (f"{number:.2f}") with that way.
-        print(f"percentage of throws of value 2 = {dicepercentage.percentage(2,repetition):.2f} %")
-        print(f"percentage of throws of value 3 = {dicepercentage.percentage(3,repetition):.2f} %")
-        print(f"percentage of throws of value 4 = {dicepercentage.percentage(4,repetition):.2f} %")
-        print(f"percentage of throws of value 5 = {dicepercentage.percentage(5,repetition):.2f} %")
-        print(f"percentage of throws of value 6 = {dicepercentage.percentage(6,repetition):.2f} %")
+        print(f"percentage of throws of value 1 = {dice_percentage.percentage(1,repetition):.2f} %")
+        print(f"percentage of throws of value 2 = {dice_percentage.percentage(2,repetition):.2f} %")
+        print(f"percentage of throws of value 3 = {dice_percentage.percentage(3,repetition):.2f} %")
+        print(f"percentage of throws of value 4 = {dice_percentage.percentage(4,repetition):.2f} %")
+        print(f"percentage of throws of value 5 = {dice_percentage.percentage(5,repetition):.2f} %")
+        print(f"percentage of throws of value 6 = {dice_percentage.percentage(6,repetition):.2f} %")
