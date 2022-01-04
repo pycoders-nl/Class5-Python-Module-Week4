8d92dbe9178202dc1dac8ce2014315f91844e556
hltaydn <91338529+hltaydn@users.noreply.github.com>
2022-01-04 13:24:33 +0100

    Update my_dice.py

--- my_dice.py
+++ my_dice.py
@@ -1,7 +1,7 @@
 from random import randint
 def rollDice(number):
-    dice=[0]*6 
+    dice=[0]*6 #[0,0,0,0,0,0]
     for i in range(number):
-        dice[randint(1,6)-1]+=1
+        dice[randint(1,6)-1]+=1 #specify the index => [randint(1,6)-1]
     dice=[round((dice[i]/number)*100,2) for i in range(len(dice))]
     return dice
